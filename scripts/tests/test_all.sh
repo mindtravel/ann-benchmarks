0441c1ce5dbfaa9f16b3b9fa0921eafa1f4f9320
mindtravel <2200013088@stu.pku.edu.cn>
2025-10-23 17:23:46 +0800

    更新脚本

--- scripts/tests/test_all.sh
+++ scripts/tests/test_all.sh
@@ -1,6 +1,5 @@
 # 用合同中的三个数据集测试
-for dataset in "TEXT1M-200-angular"
-# "sift-128-euclidean" "deep-image-96-angular" 
+for dataset in "TEXT1M-200-angular" "sift-128-euclidean" "deep-image-96-angular" 
 do
     # 测试原版pgvector
     ./scripts/tests/pgvector_origin.sh $dataset
